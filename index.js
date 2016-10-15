89e8e281fe6a62c8fdc03580f33f56e833d88317
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-10-15 00:19:04 -0300

    Method implementation and improvements
    Added delete method;
    Binded end of connection;
    Added missing "contact" param;
    Put tables name between quotes;
    Directory redefinition;
    Imports improvement;
    Correction on "pg" import.

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').Server(require('express')());
 var io = require('socket.io');
-var employee = require('./actions/employee');
-var client = require('./actions/client');
-
-var pgClient = new require('pg').Client(process.env.DATABASE_URL);
+// DATABASE_URL = postgres://{USER}:{PASSWORD}@{HOST}/{DB_NAME}
+var pgClient = new (require('pg').Client)(process.env.DATABASE_URI); // Shortened due to specific need
+var employee = require('./js/employee');
+var client = require('./js/client');
 
 const PORT = typeof(process.env.PORT) != "undefined" ? process.env.PORT : 8888;
 
 io.on("connection", function (socket) {
+    // Employee
     socket.on("create employee", function(data) {
         employee.create(pgClient, data);
     });
     socket.on("update employee", function(data) {
         employee.update(pgClient, data);
     });
+    socket.on("delete employee", function(data) {
+        employee.delete(pgClient, data);
+    });
+
+    // Client
     socket.on("create client", function(data) {
         client.create(pgClient, data);
     });
     socket.on("update client", function(data) {
         client.update(pgClient, data);
+    });
+    socket.on("delete client", function(data) {
+        client.delete(pgClient, data);
     });
 });
 
--- js/employee.js
+++ js/employee.js
@@ -1,12 +1,14 @@
 module.exports = {
     create: function (dbClient, employee_data) {
         dbClient.connect();
+        dbClient.on("drain", dbClient.end.bind(dbClient));
         var queryStmt = "insert into Employee (address, contact, cpf, role, name, observation) values ";
 
         for(var i = 0; i < employee_data.length; i++) {
             if (i != 0) queryStmt += ",";
             queryStmt += "(" +
                 employee_data.address + ", " +
+                employee_data.contact + ", " +
                 employee_data.cpf + ", " +
                 employee_data.role + ", " +
                 employee_data.name + ", " +
@@ -18,8 +20,9 @@ module.exports = {
     },
     update: function (dbClient, employee_data) {
         dbClient.connect();
+        dbClient.on("drain", dbClient.end.bind(dbClient));
         dbClient.query(
-            "update Employee set " +
+            "update \"Employee\" set " +
             "address = $1, contact = $2, cpf = $3, role = $4, name = $5, observation = $6 " +
             "where id = $7",
             [
@@ -32,5 +35,10 @@ module.exports = {
                 employee_data.id
             ]
         );
+    },
+    delete: function(dbClient, employee_data) {
+        dbClient.connect();
+        dbClient.on("drain", dbClient.end.bind(dbClient));
+        dbClient.query("delete from \"Employee\" where id = $1", [employee_data.id]);
     }
 };
