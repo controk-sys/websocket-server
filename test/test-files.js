a7d29ed7bb8b92e0e4e39ea1ebce698193921bca
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-11-13 05:23:02 -0300

    Created test file to require all files to be covered by tests.

---
+++ test/test-files.js
@@ -0,0 +1,12 @@
+// https://github.com/gotwarlost/istanbul/issues/112#issuecomment-29679800
+require("../controllers/clients");
+require("../controllers/employees");
+require("../controllers/products");
+require("../controllers/suppliers");
+require("../index");
+require("../models/Address");
+require("../models/Client");
+require("../models/Employee");
+require("../models/Product");
+require("../models/Supplier");
+require("./test-client-actions");
